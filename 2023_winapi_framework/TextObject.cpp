d952a9e8a1488307cb103001fcd967d3a1038d88
Woo Hyeok <pwh060720@gmail.com>
2023-12-13 14:16:13 +0900

    사운드 설정& 씬 전환

--- 2023_winapi_framework/TextObject.cpp
+++ 2023_winapi_framework/TextObject.cpp
@@ -12,7 +12,17 @@ TextObject::~TextObject()
 
 void TextObject::Render(HDC _dc)
 {
+	SetTextColor(_dc, RGB(0, 0, 0)); 
+	HFONT hFont = CreateFont(20, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
+		CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH, L"DungGeunMo");
+	SelectObject(_dc, hFont);
 	SetBkMode(_dc, TRANSPARENT);
+	SIZE textSize;
+	GetTextExtentPoint32(_dc, m_wText.c_str(), m_wText.length(), &textSize);
+
 	Vec2 pos = GetPos();
-	TextOut(_dc, pos.x, pos.y, m_wText.c_str(), m_wText.length());
+	pos.x -= textSize.cx / 2 - 30;  // Adjust the X position for center alignment
+	pos.y -= textSize.cy / 2;  // Adjust the Y position for center alignment
+
+	TextOut(_dc, static_cast<int>(pos.x), static_cast<int>(pos.y), m_wText.c_str(), m_wText.length());
 }
